c5d6bdd825686e0549391985b18ccbd1050b1b07
Felipe Augusto <feliperocha.ti@gmail.com>
2013-12-10 13:23:31 -0200

    remocao dos plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -61,7 +61,7 @@ grails.project.dependency.resolution = {
 
         compile ':cache:1.0.1'
 
-        compile ':heroku:1.0.1'
-        compile ':cloud-support:1.0.8'
+        //compile ':heroku:1.0.1'
+        //compile ':cloud-support:1.0.8'
     }
 }
